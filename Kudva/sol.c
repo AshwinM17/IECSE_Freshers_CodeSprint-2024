9b719b82a763404f120f9ba464e87aeb2bfc933b
Roonil03 <67649149+Roonil03@users.noreply.github.com>
2024-10-24 23:07:22 +0530

    Update sol.c

--- Kudva/sol.c
+++ Kudva/sol.c
@@ -14,17 +14,15 @@ int findDuplicate(int arr[], int n) {
 
 int main() {
     int n;
-    printf("Enter the number of elements (n): ");
     scanf("%d", &n);
 
     int arr[n + 1];
-    printf("Enter %d numbers (1 to %d) with one number repeated:\n", n + 1, n);
     for (int i = 0; i < n + 1; i++) {
         scanf("%d", &arr[i]);
     }
 
     int duplicate = findDuplicate(arr, n + 1);
-    printf("The duplicate number is: %d\n", duplicate);
+    printf("%d", duplicate);
 
     return 0;
 }
