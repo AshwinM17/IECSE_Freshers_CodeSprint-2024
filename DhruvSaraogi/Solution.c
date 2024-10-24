a0cecd68b195491e36076f5aeab387631db2adf4
DHRUV SARAOGI <155884312+DhruvSaraogi@users.noreply.github.com>
2024-10-25 00:02:10 +0530

    Update Solution.c

--- DhruvSaraogi/Solution.c
+++ DhruvSaraogi/Solution.c
@@ -11,19 +11,18 @@ int Identical(char arr1[], char t1[], int y1, char arr2[], char t2[], int y2) {
 }
 
 
-void findDuplicates(char arr[][AL], char t[][TL], int y[], int size, char r[size][TL],int k) {
-    int found = 0;
+int findDuplicates(char arr[][AL], char t[][TL], int y[], int size) {
+    int c = 0;
     
 
     for (int i = 0; i < size; i++) {
         for (int j = i + 1; j < size; j++) {
             if (Identical(arr[i], t[i], y[i], arr[j], t[j], y[j])) {
-                
-                strcpy(r[k++], t[i]);
-                strcpy(r[k++], t[j]);
+                c++; 
             }
         }
     }
+    return c;
     
 }
 
@@ -36,46 +35,21 @@ int main() {
     char arr[MR][AL];
     char t[MR][TL];
     int y[MR];
-    int k=0;
-    int l=0;
 
-    printf("\nEnter details for record \n");
     for (int i = 0; i < num; i++) {
-        printf("Artist %d :",i+1);
         scanf("%s", &arr[i]); 
     }
-    printf("\n");
     for (int i = 0; i < num; i++) {
-        printf("Title %d: ",i+1);
+        
         scanf("%s", &t[i]); 
     }
-    printf("\n");
     for (int i = 0; i < num; i++) {
-         printf("Year %d: ",i+1);
         scanf("%d", &y[i]);  
     }
     
-    char r[num][TL];  
     
-    findDuplicates(arr, t, y, num, r,k);
+    printf("%d",findDuplicates(arr, t, y, num));
 
-    if (strlen(r[0]) > 0) {
-        l=sizeof(r)/sizeof(r[0]);
-        for(int i=0;i<l;i++){
-            if(i==0){
-               printf("['%s',",r[i]);
-            }
-            else if(i==l-1){
-               printf("'%s']",r[i]);
-            }
-            else{
-               printf("'%s',",r[i]);
-            }
-           
-        }
-    } else {
-        printf("\nNo duplicates found.\n");
-    }
 
     return 0;
 }
