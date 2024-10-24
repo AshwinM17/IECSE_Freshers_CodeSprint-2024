078d7ae0ca7e1851b4e4131ce9e44648943c980d
Hitansh Agarwal <66518468+HitanshAgarwal@users.noreply.github.com>
2024-10-24 13:53:56 +0530

    Add files via upload

---
+++ Hitansh/solution.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+    // Create points vector to store all x-coordinates and heights
+    vector<pair<int, int>> points;
+    for(const auto& b : buildings) {
+        points.push_back({b[0], -b[2]});  
+        points.push_back({b[1], b[2]});   
+    }
+
+    sort(points.begin(), points.end());
+    
+    multiset<int> heights;
+    heights.insert(0);
+    
+    vector<vector<int>> skyline;
+    int prevHeight = 0;
+    
+    for(const auto& p : points) {
+        int x = p.first;
+        int h = p.second;
+        
+        if(h < 0) {
+            heights.insert(-h);
+        }
+        else {
+            heights.erase(heights.find(h));
+        }
+        
+        int currentHeight = *heights.rbegin();
+        
+        if(currentHeight != prevHeight) {
+            skyline.push_back({x, currentHeight});
+            prevHeight = currentHeight;
+        }
+    }
+    
+    return skyline;
+}
+
+
+void printSkyline(const vector<vector<int>>& skyline) {
+    cout << "Skyline points: ";
+    for(const auto& point : skyline) {
+        cout << "[" << point[0] << "," << point[1] << "] ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<vector<int>> buildings(n, vector<int>(3));
+    
+    for(int i = 0; i < n; i++) {
+        cin >> buildings[i][0] >> buildings[i][1] >> buildings[i][2];
+    }
+    
+    vector<vector<int>> result = getSkyline(buildings);
+    printSkyline(result);
+    
+    return 0;
+}
